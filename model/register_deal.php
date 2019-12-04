537a96a0c3672c0926870feb86aa76c8113779da
Bobby Rong <rjl931189261@126.com>
2019-12-04 12:38:05 +0800

    register_deal.php：注册信息检查

--- model/register_deal.php
+++ model/register_deal.php
@@ -3,3 +3,20 @@
 $user_name = trim($_POST['user_name']);
 $user_password1 = trim($_POST['user_password1']);
 $user_password2 = trim($_POST['user_password2']);
+
+// 检查注册信息是否填写完整，是否符合要求
+if ( empty($user_name) ) {
+	jump('./register.php', '请填写用户名');
+}
+else if ( empty($user_password1) || empty($user_password2) ) {
+	jump('./register.php', '请填写密码');
+}
+else if ( strlen($user_name) < 6 || strlen($user_name > 10) ) {
+	jump('./register.php', '用户名长度不符要求');
+}
+else if ( $user_password1 !== $user_password2 ) {
+	jump('./register.php', '两次密码输入不一致');
+}
+else if ( strlen($user_password1) < 6  ) {
+	jump('./register.php', '密码长度不符要求');
+}
