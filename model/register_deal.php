104cb9c5d0204bbaa76e0b73ba80188091efd1c8
Bobby Rong <rjl931189261@126.com>
2019-12-05 14:25:33 +0800

    数据库连接支持

--- model/register_deal.php
+++ model/register_deal.php
@@ -1,4 +1,5 @@
 <?php
+include  '../core/mysqlDB.php';
 // 接收注册信息
 $user_name = trim($_POST['user_name']);
 $user_password1 = trim($_POST['user_password1']);
--- model/validate.php
+++ model/validate.php
@@ -1,4 +1,5 @@
 <?php
+include  '../core/mysqlDB.php';
 // 接收登录信息
 $user_name = $_POST['user_name'];
 $user_password = $_POST['user_password'];
