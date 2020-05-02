207f1b5e9782900ef8a16017d6c55751718dbcda
Bobby Rong <rjl931189261@126.com>
2020-05-02 20:20:05 +0800

    注销功能基本实现

---
+++ controller/logoutAction.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    // header('Content-type:text/html;charset=utf-8');
+    if(isset($_SESSION['user'])){
+        session_unset();//free all session variable
+        session_destroy();//销毁一个会话中的全部数据
+        // setcookie(session_name(),'',time()-3600);
+		echo '<script> alert(\'注销成功~~\');location.href=\'../login.php\';</script>';
+		exit;
+    }else{
+        echo '<script> alert(\'注销失败，请重新尝试~~\');javascript:history.back(-1);</script>';
+		exit;
+    }
