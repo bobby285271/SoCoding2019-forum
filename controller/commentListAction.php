3639443997a489bbf7025301d4f09098639b2045
Bobby Rong <rjl931189261@126.com>
2020-05-02 18:18:49 +0800

    查看评论部分基本实现

---
+++ controller/commentListAction.php
@@ -0,0 +1,22 @@
+<?php
+
+// 公共函数库已在别处加载
+// include 'function.php';
+
+
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM {$comTable}";
+$conn = db_connect($db);
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // 输出数据
+    while($row = mysqli_fetch_assoc($result)) {
+        if($row["id"] == $id) {
+            echo "<li class=\"list-group-item\">"  .$row["account"]. "：" .$row["content"]. "</li>";
+        }
+        
+    }
+} else {
+    echo "0 结果";
+}
--- view.php
+++ view.php
@@ -7,3 +7,5 @@ include './view/view-head.html';
 echo "<a href=\"controller/delAction.php?id=" .$id. "\">删除帖子</a> <a href=\"edit.php?id=" .$id. "\">编辑帖子</a><br /><br />";
 include "./controller/detailsAction.php";
 include './view/view-mid.html';
+include "./controller/commentListAction.php";
+include './view/view-foot.html';
