d387339f0e0c3102ea3840d393a832c93e214dfe
Bobby Rong <rjl931189261@126.com>
2020-05-03 13:51:52 +0800

    移除 0.1 遗留多余文件

--- controller/backup/register_deal.php
+++ controller/backup/register_deal.php
@@ -1,47 +1,47 @@
 <?php
-include  '../core/mysqlDB.php';
-// 接收注册信息
-$user_name = trim($_POST['user_name']);
-$user_password1 = trim($_POST['user_password1']);
-$user_password2 = trim($_POST['user_password2']);
+// include  '../core/mysqlDB.php';
+// // 接收注册信息
+// $user_name = trim($_POST['user_name']);
+// $user_password1 = trim($_POST['user_password1']);
+// $user_password2 = trim($_POST['user_password2']);
 
-// 设置编码，防止中文乱码
-mysqli_query($conn , "set names utf8");
+// // 设置编码，防止中文乱码
+// mysqli_query($conn , "set names utf8");
 
-// 查找当前提交的用户名
-$sql = "SELECT * FROM user WHERE user_name = '$user_name'";
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($result);
+// // 查找当前提交的用户名
+// $sql = "SELECT * FROM user WHERE user_name = '$user_name'";
+// $result = mysqli_query($conn, $sql);
+// $row = mysqli_fetch_assoc($result);
 
-// 检查注册信息是否填写完整，是否符合要求
-if ( empty($user_name) ) {
-	jump('./register.php', '请填写用户名');
-}
-else if ( empty($user_password1) || empty($user_password2) ) {
-	jump('./register.php', '请填写密码');
-}
-else if ( strlen($user_name) < 6 || strlen($user_name > 10) ) {
-	jump('./register.php', '用户名长度不符要求');
-}
-else if ( $user_password1 !== $user_password2 ) {
-	jump('./register.php', '两次密码输入不一致');
-}
-else if ( strlen($user_password1) < 6  ) {
-	jump('./register.php', '密码长度不符要求');
-}
-else if ( mysqli_num_rows($result) != 0 ) {
-	jump('./login.php', '用户名已存在');
-}
-else {
-	// 用 MD5 加密用户密码后写入数据库
-	$user_password = md5($user_password1);
-	$sql = "INSERT INTO user (user_name, user_password)
-		VALUES ('$user_name', '$user_password')";
-	if ( mysqli_query($conn, $sql) ) {
-		mysqli_close($conn);
-		jump('./login.php', '注册成功');
-	}
-	else {
-		jump('./register.php', '注册失败');
-	}
-}
+// // 检查注册信息是否填写完整，是否符合要求
+// if ( empty($user_name) ) {
+// 	jump('./register.php', '请填写用户名');
+// }
+// else if ( empty($user_password1) || empty($user_password2) ) {
+// 	jump('./register.php', '请填写密码');
+// }
+// else if ( strlen($user_name) < 6 || strlen($user_name > 10) ) {
+// 	jump('./register.php', '用户名长度不符要求');
+// }
+// else if ( $user_password1 !== $user_password2 ) {
+// 	jump('./register.php', '两次密码输入不一致');
+// }
+// else if ( strlen($user_password1) < 6  ) {
+// 	jump('./register.php', '密码长度不符要求');
+// }
+// else if ( mysqli_num_rows($result) != 0 ) {
+// 	jump('./login.php', '用户名已存在');
+// }
+// else {
+// 	// 用 MD5 加密用户密码后写入数据库
+// 	$user_password = md5($user_password1);
+// 	$sql = "INSERT INTO user (user_name, user_password)
+// 		VALUES ('$user_name', '$user_password')";
+// 	if ( mysqli_query($conn, $sql) ) {
+// 		mysqli_close($conn);
+// 		jump('./login.php', '注册成功');
+// 	}
+// 	else {
+// 		jump('./register.php', '注册失败');
+// 	}
+// }
--- controller/backup/validate.php
+++ controller/backup/validate.php
@@ -1,42 +1,42 @@
 <?php
-include  '../core/mysqlDB.php';
+// include  '../core/mysqlDB.php';
 
-// 接收登录信息
-$user_name = $_POST['user_name'];
-$user_password = $_POST['user_password'];
+// // 接收登录信息
+// $user_name = $_POST['user_name'];
+// $user_password = $_POST['user_password'];
 
-// 设置编码，防止中文乱码
-mysqli_query($conn, "set names utf8");
+// // 设置编码，防止中文乱码
+// mysqli_query($conn, "set names utf8");
 
-// 查找当前提交的用户名
-$sql = "SELECT * FROM user WHERE user_name = '$user_name'";
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($result);
+// // 查找当前提交的用户名
+// $sql = "SELECT * FROM user WHERE user_name = '$user_name'";
+// $result = mysqli_query($conn, $sql);
+// $row = mysqli_fetch_assoc($result);
 
-// 提取该用户的真实密码
-$real_password = $row['user_password'];
-mysqli_close($conn);
+// // 提取该用户的真实密码
+// $real_password = $row['user_password'];
+// mysqli_close($conn);
 
-// 检查用户密码是否填写完整
-if ( empty($user_name) ) {
-	jump('./login.php', '用户名不能为空');
-}
-else if ( empty($user_password) ) {
-	jump('./login.php', '密码不能为空');
-}
-else if ( mysqli_num_rows($result) == 0 ) {
-	jump('./login.php', '用户名不存在');
-}
-// 比对密码的 MD5 是否一致
-else if ( md5($user_password) === $real_password ) {
-	// TODO：保存登录状态
-	$expire=time()+60*3;//cookie的保存时间60秒*3分钟
-	setcookie("user_name", $user_name, $expire,"/");
-	//还可以set一个加密后的user_password来防止伪造cookie
-	//在登陆后用 $_COOKIE["user_name"] 来判断是否已经登录过
-	jump('../index.php', '登录成功');
-}
-else{
-	jump('./login.php', '密码错误');
-}
+// // 检查用户密码是否填写完整
+// if ( empty($user_name) ) {
+// 	jump('./login.php', '用户名不能为空');
+// }
+// else if ( empty($user_password) ) {
+// 	jump('./login.php', '密码不能为空');
+// }
+// else if ( mysqli_num_rows($result) == 0 ) {
+// 	jump('./login.php', '用户名不存在');
+// }
+// // 比对密码的 MD5 是否一致
+// else if ( md5($user_password) === $real_password ) {
+// 	// TODO：保存登录状态
+// 	$expire=time()+60*3;//cookie的保存时间60秒*3分钟
+// 	setcookie("user_name", $user_name, $expire,"/");
+// 	//还可以set一个加密后的user_password来防止伪造cookie
+// 	//在登陆后用 $_COOKIE["user_name"] 来判断是否已经登录过
+// 	jump('../index.php', '登录成功');
+// }
+// else{
+// 	jump('./login.php', '密码错误');
+// }
 
