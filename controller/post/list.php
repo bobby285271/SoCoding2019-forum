44c201eba30ab10df98ae0ba736c9ba0a2f8f79e
Bobby Rong <rjl931189261@126.com>
2020-05-05 17:47:49 +0800

    分页显示功能初始化

--- controller/post/list.php
+++ controller/post/list.php
@@ -1,17 +1,21 @@
 <?php
 
 //1.加载公共函数库
-include '../public/function.php';
+include './controller/public/function.php';
 
-$sql = "SELECT * FROM {$postTable}";
+$page = $_GET['page'];
+
+$page = ($page-1)*10;
+
+$sql = "SELECT * FROM {$postTable} LIMIT {$page},10";
 $conn = db_connect($db);
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // 输出数据
     while($row = mysqli_fetch_assoc($result)) {
         // echo "id: " . $row["user_account"]. " <br>";
-        echo "document.write('<a href=\"index.php?action=view&id=" .$row["id"]. "\" class=\"list-group-item list-group-item-action\">" .$row["post_title"]. "</a>');";
+        echo "<a href=\"index.php?action=view&id=" .$row["id"]. "\" class=\"list-group-item list-group-item-action\">" .$row["post_title"]. "</a>";
     }
 } else {
-    echo "document.write('<div class=\"card\"><div class=\"card-body\">暂时还没有帖子。</div></card>');";
+    echo "<div class=\"card\"><div class=\"card-body\">暂时还没有帖子。</div></card>";
 }
